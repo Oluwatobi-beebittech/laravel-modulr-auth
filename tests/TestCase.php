4a6e0bd1fc08d2d185cf10ff78645d4d0843a6cf
Oluwatobi Akanji <tobi@542.digital>
2022-07-23 23:10:43 +0100

    test: initial setup for test

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Laravel Modulr Auth package.
+ *
+ * (c) Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OluwatobiAkanji\ModulrAuth\Tests;
+
+use OluwatobiAkanji\ModulrAuth\ModulrAuthServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+  public function setUp(): void
+  {
+    parent::setUp();
+  }
+
+  protected function getPackageProviders($app)
+  {
+    return [
+      ModulrAuthServiceProvider::class,
+    ];
+  }
+
+  protected function getEnvironmentSetUp($app)
+  {
+    // perform environment setup
+  }
+}
