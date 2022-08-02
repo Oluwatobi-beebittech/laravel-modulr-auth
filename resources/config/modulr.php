bae2bc9e1facde23027dd1c55d03112a7b1c7669
Oluwatobi Akanji <tobi@542.digital>
2022-08-02 16:16:36 +0100

    fix: added post package install script

--- resources/config/modulr.php
+++ resources/config/modulr.php
@@ -11,14 +11,34 @@
 
 return [
     
+     /**
+     * API key From Modulr
+     *
+     */
     'key' => env('MODULR_API_KEY'),
 
+    /**
+     * API secret From Modulr
+     *
+     */
     'secret' => env('MODULR_API_SECRET'),
 
+    /**
+     * Modulr API base url
+     *
+     */
     'base_url' => env('MODULR_BASE_URL', 'https://api-sandbox.modulrfinance.com'),
 
-    'live_base_route' => env('MODULR_LIVE_BASE_ROUTE', '/api-live'),
+    /**
+     * Modulr live base route
+     *
+     */
+    'live_base_route' => env('MODULR_LIVE_BASE_ROUTE'),
 
+    /**
+     * Modulr sandbox base route
+     *
+     */
     'local_base_route' => env('MODULR_LOCAL_BASE_ROUTE', '/api-sandbox'),
 
 ];
